0ff22cbc41696aa61b20fd7ec10b6962382c27d1
Ye Naing Tun <yenaingtun425@gmail.com>
2024-07-02 21:52:04 +0630

    added tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button  onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
