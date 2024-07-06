45cbf550fa8946323f11a89b9d721c7e9d398788
Ye Naing Tun <yenaingtun425@gmail.com>
2024-07-06 23:00:01 +0630

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,16 @@ import HighestScore from "./Components/HighestScore";
 
 const App = () => {
   return (
-    <>
+    <section className="max-w-7xl p-4 bg-teal-400">
       <h1 className="text-xl text-black">Header</h1>
-      <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 content-start  place-items-start">
+      <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 content-start place-items-start">
         <Cards  />
         <div className="bg-green-300">
           <CurrentScore />
           <HighestScore />
         </div>
       </div>
-    </>
+    </section>
   );
 };
 
