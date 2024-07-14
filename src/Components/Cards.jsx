cedc825ead0cbb9d3705b2eae96286cb508fe32b
Ye Naing Tun <yenaingtun425@gmail.com>
2024-07-14 21:49:58 +0630

    updated cards

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,24 +1,43 @@
 import React from "react";
 
 const Cards = () => {
+  const tees = [
+    {
+      id: 1,
+      title: "Basic Tee",
+    },
+    {
+      id: 2,
+      title: "Basic Tee",
+    },
+    {
+      id: 3,
+      title: "Basic Tee",
+    },
+  ];
+
+  const listItems = tees.map((tee) => (
+    <li>
+      <a href="#" className="group block overflow-hidden rounded-lg">
+        <img
+          src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+          alt=""
+          className="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[400px]"
+        />
+        <div className="relative bg-white py-2">
+          <h3 className="text-sm text-center text-gray-700 group-hover:underline group-hover:underline-offset-4">
+            {tee.title}
+          </h3>
+        </div>
+      </a>
+    </li>
+  ));
+
   return (
     <section className="text-gray-600 body-font col-span-2 lg:col-span-3">
       <div className="container px-5 mx-auto">
         <ul className="grid lg:gap-4 grid-cols-1 lg:grid-cols-4">
-          <li>
-            <a href="#" className="group block overflow-hidden rounded-lg">
-              <img
-                src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                alt=""
-                className="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[400px]"
-              />
-              <div className="relative bg-white py-2">
-                <h3 className="text-sm text-center text-gray-700 group-hover:underline group-hover:underline-offset-4">
-                  Basic Tee
-                </h3>
-              </div>
-            </a>
-          </li>
+          {listItems}
         </ul>
       </div>
     </section>
